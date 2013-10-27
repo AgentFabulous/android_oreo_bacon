933271071facab19bcb7b422fd0f6cd673e0179e
Shuzhen Wang <shuzhenw@codeaurora.org>
2013-10-27 01:43:50 -0700

    Camera: HAL1: Remove AF state machine.
    To make HAL1 logic consistent with HAL3, remove the AF state
    machine from HAL. AF algorithm would take care of the state
    machine logic.
    
    Change-Id: I9757ec6e6f7cadeca351ff3583ac757fe44881a3

--- QCamera2/HAL/QCameraParameters.cpp
+++ QCamera2/HAL/QCameraParameters.cpp
@@ -611,9 +611,6 @@ QCameraParameters::QCameraParameters()
       m_bNeedRestart(false),
       m_bNoDisplayMode(false),
       m_bWNROn(false),
-      m_bNeedLockCAF(false),
-      m_bCAFLocked(false),
-      m_bAFRunning(false),
       m_bInited(false),
       m_nBurstNum(1),
       m_bUpdateEffects(false),
@@ -688,9 +685,6 @@ QCameraParameters::QCameraParameters(const String8 &params)
     m_bNeedRestart(false),
     m_bNoDisplayMode(false),
     m_bWNROn(false),
-    m_bNeedLockCAF(false),
-    m_bCAFLocked(false),
-    m_bAFRunning(false),
     m_bInited(false),
     m_nBurstNum(1),
     m_bPreviewFlipChanged(false),
@@ -4500,11 +4494,6 @@ int32_t QCameraParameters::setFocusMode(const char *focusMode)
             ALOGV("%s: Setting focus mode %s", __func__, focusMode);
             mFocusMode = (cam_focus_mode_type)value;
 
-            // reset need lock CAF flag
-            m_bNeedLockCAF = false;
-            m_bCAFLocked = false;
-            m_bAFRunning = false;
-
             updateParamEntry(KEY_FOCUS_MODE, focusMode);
             rc = AddSetParmEntryToBatch(m_pParamBuf,
                                           CAM_INTF_PARM_FOCUS_MODE,
@@ -7265,46 +7254,6 @@ int32_t QCameraParameters::setFaceDetection(bool enabled)
     ALOGD("%s: FaceProcMask -> %d", __func__, m_nFaceProcMask);
 
     return rc;
-}
-
-/*===========================================================================
- * FUNCTION   : setLockCAF
- *
- * DESCRIPTION: Lock CAF
- *
- * PARAMETERS :
- *   @bLock : if CAF needs to be locked
- *
- * RETURN     : int32_t type of status
- *              NO_ERROR  -- success
- *              none-zero failure code
- *==========================================================================*/
-int32_t QCameraParameters::setLockCAF(bool bLock)
-{
-    if(initBatchUpdate(m_pParamBuf) < 0 ) {
-        ALOGE("%s:Failed to initialize group update table", __func__);
-        return BAD_TYPE;
-    }
-    int32_t rc = NO_ERROR;
-    int32_t value = bLock;
-
-    rc = AddSetParmEntryToBatch(m_pParamBuf,
-                                CAM_INTF_PARM_LOCK_CAF,
-                                sizeof(value),
-                                &value);
-    if (rc != NO_ERROR) {
-        ALOGE("%s:Failed to update table", __func__);
-        return rc;
-    }
-
-    rc = commitSetBatch();
-    if (rc != NO_ERROR) {
-        ALOGE("%s:Failed to set lock CAF parm", __func__);
-        return rc;
-    } else {
-        m_bCAFLocked = bLock;
-        return NO_ERROR;
-    }
 }
 
 /*===========================================================================
