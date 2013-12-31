fb9703943d3b6371bd74d699ca5dc723167f2a55
Lokesh Kumar Aakulu <lkumar@codeaurora.org>
2013-12-31 17:43:52 +0530

    mm-qcamera-test: Avoid Tune data reset when capture snapshot
    when issue Snapshot capture command back end will load exisisting
    chromatix header from shared library. Due to this tuned data is
    reset. To avoid this scenario we call reload chromatix after
    restart preview in snapshot process.
    
    Change-Id: Iacd10ff4c7e2476ab069e53a7ceb8608d15125ac

--- QCamera2/stack/mm-camera-test/inc/mm_qcamera_app.h
+++ QCamera2/stack/mm-camera-test/inc/mm_qcamera_app.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012, 2014, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -244,6 +244,8 @@ typedef struct {
     mm_camera_queue_t pp_frames;
     mm_camera_stream_t *reproc_stream;
     cam_metadata_info_t *metadata;
+    int8_t is_chromatix_reload;
+    tune_chromatix_t tune_data;
 } mm_camera_test_obj_t;
 
 typedef struct {
