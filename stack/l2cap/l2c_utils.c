b2386cb6e196487f2cc703d84bb1815eb1c5fc62
Nitin Arora <niarora@codeaurora.org>
2014-06-06 15:21:25 -0700

    Reset LE connecting flag on disconnect only if BDA matches
    This change will make sure that the flag representing that BLE device
    is connecting is made false only if a disconnection of the same
    LE device is received, whose connection is in progress. This is ensured
    by matching the bda of the disconnecting device and the LE device which
    is in connecting state. This is to prevent a BR/EDR disconnection from
    cancelling the connection timeout of an LE device
    
    Change-Id: Ia5b47681479dc0be217cdfc3e15af9ca5323575c

--- stack/l2cap/l2c_utils.c
+++ stack/l2cap/l2c_utils.c
@@ -177,7 +177,9 @@ void l2cu_release_lcb (tL2C_LCB *p_lcb)
     }
 
 #if (BLE_INCLUDED == TRUE)
-    l2cb.is_ble_connecting = FALSE;
+    // Reset BLE connecting flag only if the address matches
+    if (!memcmp(l2cb.ble_connecting_bda, p_lcb->remote_bd_addr, BD_ADDR_LEN))
+        l2cb.is_ble_connecting = FALSE;
 #endif
 
 #if (L2CAP_NUM_FIXED_CHNLS > 0)
