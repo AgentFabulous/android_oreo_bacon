9c264e92305f84a5e8a92bc859812def0ce71792
Anubhav Gupta <anubhavg@codeaurora.org>
2014-07-14 18:53:37 +0530

    Set the proper authentication level for AvOpen
    During Avopen security was made none hence A2DP
    connection was happening without bonding when
    DUT is paired with DUT and then pairing entry
    is deleted from remote and then only A2dp is
    tried to be reconnected from DUT. A2dp appears
    to be connected in this usecase but the device
    entry is shown in available devices list instead
    of paired one.
    Changes made from No security to SEC Authenticate
    
    Change-Id: I7fc39fe8acef874f0031c9b78d4269a62b94e4b0

--- btif/src/btif_av.c
+++ btif/src/btif_av.c
@@ -300,13 +300,13 @@ static BOOLEAN btif_av_state_idle_handler(btif_sm_event_t event, void *p_data)
                  memcpy(&btif_av_cb.peer_bda, ((btif_av_connect_req_t*)p_data)->target_bda,
                                                                    sizeof(bt_bdaddr_t));
                  BTA_AvOpen(btif_av_cb.peer_bda.address, btif_av_cb.bta_handle,
-                    TRUE, BTA_SEC_NONE, ((btif_av_connect_req_t*)p_data)->uuid);
+                    TRUE, BTA_SEC_AUTHENTICATE, ((btif_av_connect_req_t*)p_data)->uuid);
              }
              else if (event == BTA_AV_PENDING_EVT)
              {
                   bdcpy(btif_av_cb.peer_bda.address, ((tBTA_AV*)p_data)->pend.bd_addr);
                   BTA_AvOpen(btif_av_cb.peer_bda.address, btif_av_cb.bta_handle,
-                    TRUE, BTA_SEC_NONE, UUID_SERVCLASS_AUDIO_SOURCE);
+                    TRUE, BTA_SEC_AUTHENTICATE, UUID_SERVCLASS_AUDIO_SOURCE);
              }
              btif_sm_change_state(btif_av_cb.sm_handle, BTIF_AV_STATE_OPENING);
         } break;
