39e8abd1f33dbc7cd3514b2af9c8226bc492e07f
Sharvil Nanavati <sharvil@google.com>
2014-08-29 18:26:23 -0700

    Fix memory leak in RFCOMM sockets where some lists were not being freed.

--- btif/src/btif_sock_rfc.c
+++ btif/src/btif_sock_rfc.c
@@ -117,12 +117,11 @@ void btsock_rfc_cleanup(void) {
   pth = -1;
 
   lock_slot(&slot_lock);
-  for (size_t i = 0; i < ARRAY_SIZE(rfc_slots); ++i)
-    if (rfc_slots[i].id) {
+  for (size_t i = 0; i < ARRAY_SIZE(rfc_slots); ++i) {
+    if (rfc_slots[i].id)
       cleanup_rfc_slot(&rfc_slots[i]);
-      list_free(rfc_slots[i].incoming_queue);
-      rfc_slots[i].incoming_queue = NULL;
-    }
+    list_free(rfc_slots[i].incoming_queue);
+  }
   unlock_slot(&slot_lock);
 }
 
