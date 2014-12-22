764babe943fc2e559923c7bf8514664187808ddd
Nitin Srivastava <nitinsr@codeaurora.org>
2014-12-22 19:54:09 +0530

    Protect reset of out stream with mutex
    Protect reset of out stream with mutex so that the access
    of the same is properly synchronized in multi-threaded environment.
    
    Change-Id: I6dd81d8c7fa219ee8d1e4d82aa1e7ec52dcecec8

--- audio_a2dp_hw/audio_a2dp_hw.c
+++ audio_a2dp_hw/audio_a2dp_hw.c
@@ -1082,12 +1082,14 @@ static void adev_close_output_stream(struct audio_hw_device *dev,
 
     INFO("closing output (state %d)", out->common.state);
 
+    pthread_mutex_lock(&out->common.lock);
     if ((out->common.state == AUDIO_A2DP_STATE_STARTED) || (out->common.state == AUDIO_A2DP_STATE_STOPPING))
         stop_audio_datapath(&out->common);
 
     skt_disconnect(out->common.ctrl_fd);
     free(stream);
     a2dp_dev->output = NULL;
+    pthread_mutex_unlock(&out->common.lock);
 
     DEBUG("done");
 }
