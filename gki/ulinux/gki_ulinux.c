b9164f414b4d8d414358d0146a8f911207af3122
Elliott Hughes <enh@google.com>
2015-01-28 15:31:26 -0800

    Add missing includes.
    Change-Id: I041a5ea84b82155b0a7a8c4e7dc416c2c2b5e251

--- gki/ulinux/gki_ulinux.c
+++ gki/ulinux/gki_ulinux.c
@@ -27,6 +27,8 @@
 
 #include <assert.h>
 #include <errno.h>
+#include <malloc.h>
+#include <string.h>
 #include <sys/times.h>
 
 #include "gki_int.h"
--- osi/src/reactor.c
+++ osi/src/reactor.c
@@ -21,6 +21,7 @@
 #include <assert.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/eventfd.h>
 #include <sys/select.h>
 #include <utils/Log.h>
