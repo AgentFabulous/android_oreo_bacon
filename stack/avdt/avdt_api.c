d9279cc38b69d237ba56d7488b1da3b55e071811
VenkatRaghavan VijayaRaghavan <venkatrv@broadcom.com>
2015-02-10 07:35:10 -0800

    Resolved trace level issue for AVDTP
    Root Cause: Trace messages are getting logged
    
    When setting TRC_AVDT=5 the trace level doesn't take effect. Due to
    multiple initialization points the bt_stack.conf value was overwritten
    
    Bug: 19417758

--- stack/avdt/avdt_api.c
+++ stack/avdt/avdt_api.c
@@ -135,11 +135,6 @@ void AVDT_Register(tAVDT_REG *p_reg, tAVDT_CTRL_CBACK *p_cback)
     avdt_scb_init();
     avdt_ccb_init();
     avdt_ad_init();
-#if defined(AVDT_INITIAL_TRACE_LEVEL)
-    avdt_cb.trace_level = AVDT_INITIAL_TRACE_LEVEL;
-#else
-    avdt_cb.trace_level = BT_TRACE_LEVEL_NONE;
-#endif
 
     /* copy registration struct */
     memcpy(&avdt_cb.rcb, p_reg, sizeof(tAVDT_REG));
