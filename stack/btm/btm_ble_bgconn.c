64b85064dcce04e5ee51840ce304a40fde93e3a2
kschulz <k.schulz@samsung.com>
2015-02-11 14:41:51 +0100

    BT: fix possible out of bounds read
    In btm_ble_bgconn.c, function btm_update_bg_conn_list() if the last element in
    "p_cb->bg_dev_list" is removed, the line:
    
    "p_next = p_bg_dev + 1;"
    
    will get "p_next" to point at an entry outside the bounds of the array.
    There check for "p_next->in_use" may render a TRUE result depending on what
    data exist in the memory pointed to by "p_next", which is unknown.
    If the check passes, the sentence:
    
    " memcpy(p_cur, p_next, sizeof(tBTM_LE_BG_CONN_DEV));"
    
    will copy random data into the entry pointed at by "p_cur".
    This can potentially corrupt the white list filling it with entries that
    contain garbage and not allowing to add new valid devices to the list.
    
    Bug: 18748601
    Change-Id: I98b4be4e9f4568ea77f89146eae1e38f19fa1ee3

--- stack/btm/btm_ble_bgconn.c
+++ stack/btm/btm_ble_bgconn.c
@@ -323,15 +323,18 @@ BOOLEAN btm_update_bg_conn_list(BOOLEAN to_add, BD_ADDR bd_addr)
             if (!to_add)
             {
                 memset(p_bg_dev, 0, sizeof(tBTM_LE_BG_CONN_DEV));
-                p_cb->bg_dev_num --;
-                p_cur = p_bg_dev;
-                p_next = p_bg_dev + 1;
-                for (j = i + 1; j < BTM_BLE_MAX_BG_CONN_DEV_NUM && p_next->in_use;
-                     j ++, p_cur ++, p_next ++)
-                {
-                    memcpy(p_cur, p_next, sizeof(tBTM_LE_BG_CONN_DEV));
-                    memset(p_next, 0, sizeof(tBTM_LE_BG_CONN_DEV));
+                if (p_cb->bg_dev_num < BTM_BLE_MAX_BG_CONN_DEV_NUM)
+                {/* The entry being removed is not at the highest index of the array: shift */
+                    p_cur = p_bg_dev;
+                    p_next = p_bg_dev + 1;
+                    for (j = i + 1 ;j < BTM_BLE_MAX_BG_CONN_DEV_NUM && p_next->in_use ;
+                                            j ++, p_cur ++, p_next ++ )
+                    {
+                        memcpy(p_cur, p_next, sizeof(tBTM_LE_BG_CONN_DEV));
+                        memset(p_next, 0, sizeof(tBTM_LE_BG_CONN_DEV));
+                    }
                 }
+                p_cb->bg_dev_num --;
             }
             ret = TRUE;
             break;
