a54ca891a6f2ab54f1e49b7a04a4096fa5c6edc5
Andre Eisenbach <eisenbach@google.com>
2015-02-25 16:01:07 -0800

    Revert "Remove unused inquiry variables"
    Still used by BTA API BTA_DmSetScanParam
    
    This reverts commit d84f0c8c5eb67a45cb2e0993324d1dff8b499b1b.

--- bta/dm/bta_dm_act.c
+++ bta/dm/bta_dm_act.c
@@ -558,13 +558,13 @@ void bta_dm_set_visibility (tBTA_DM_MSG *p_data)
     /* set modes for Discoverability and connectability if not ignore */
     if (p_data->set_visibility.disc_mode != BTA_DM_IGNORE)
         BTM_SetDiscoverability((UINT8)p_data->set_visibility.disc_mode,
-                                0,
-                                0);
+                                bta_dm_cb.inquiry_scan_window,
+                                bta_dm_cb.inquiry_scan_interval);
 
     if (p_data->set_visibility.conn_mode != BTA_DM_IGNORE)
         BTM_SetConnectability((UINT8)p_data->set_visibility.conn_mode,
-                                0,
-                                0);
+                                bta_dm_cb.page_scan_window,
+                                bta_dm_cb.page_scan_interval);
 
     /* Send False or True if not ignore */
     if (p_data->set_visibility.pair_mode != BTA_DM_IGNORE )
