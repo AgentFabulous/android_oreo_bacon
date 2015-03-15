f1f0e60ab939c158fb3ba5b3f9ed26fe884b93e1
Amarnath Hullur Subramanyam <amarnath@qca.qualcomm.com>
2015-03-14 20:35:27 -0700

    wpa_supplicant_private_lib: driver_nl80211.h inclusion changed
    driver_nl80211.h present in libhardware_legacy is moved to wpa_supplicant
    project. This change is to resolve the inclusion in wpa_supplicant_private
    library
    
    Change-Id: I51ef2627e68dea786e9fe0152c1fea230c433396

--- qcwcn/wpa_supplicant_8_lib/driver_cmd_nl80211.c
+++ qcwcn/wpa_supplicant_8_lib/driver_cmd_nl80211.c
@@ -10,7 +10,14 @@
  *
  */
 
-#include "hardware_legacy/driver_nl80211.h"
+#include "includes.h"
+#include <sys/types.h>
+#include <fcntl.h>
+#include <net/if.h>
+
+#include "common.h"
+#include "linux_ioctl.h"
+#include "driver_nl80211.h"
 #include "wpa_supplicant_i.h"
 #include "config.h"
 #ifdef ANDROID
