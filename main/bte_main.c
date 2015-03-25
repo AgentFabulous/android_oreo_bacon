03f182be7f2cdf06568bf046e1519ed962c89c46
Chris Manton <cmanton@google.com>
2015-03-25 18:01:11 +0000

    Revert "Resolved hardware error observed during SCO Connection setup"
    This reverts commit 4d8ac4ff015ff87c4e985ac0d9a5f3c2a6b0d9fa.
    
    Change-Id: Ie1433c82014b6ddce78c5363100ab109ae0fecde

--- main/bte_main.c
+++ main/bte_main.c
@@ -55,7 +55,6 @@
 #include "osi/include/log.h"
 #include "stack_config.h"
 #include "osi/include/thread.h"
-#include "vendor.h"
 
 /*******************************************************************************
 **  Constants & Macros
@@ -291,7 +290,7 @@ int set_audio_state(UINT16 handle, UINT16 codec, UINT8 state, void *param)
     /* layer_specific shall contain return path event! for BTA events!
      * 0 means no return message is expected. */
     p_msg->hdr.layer_specific = 0;
-    vendor_get_interface()->send_command(VENDOR_SET_AUDIO_STATE, &(p_msg->audio));
+    hci->transmit_downward(MSG_STACK_TO_HC_HCI_CMD, p_msg);
     return result;
 }
 
