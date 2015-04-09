5f0131ddab9c40d1734116d3450731b4115a31a1
Subhani Shaik <subhanis@codeaurora.org>
2015-04-09 11:48:33 +0530

    Wi-Fi HAL: Expect the link layer stats synchronously
    Expect the results synchronously and avoid the asynchronous events
    for wifi_get_link_stats. This is to make sure that all the
    vendor commands use requestResponse mechanism only
    
    Change-Id: Id4cb7149e24b342a5b84c6900dc8db2965993124

--- qcwcn/wifi_hal/llstatscommand.h
+++ qcwcn/wifi_hal/llstatscommand.h
@@ -54,15 +54,6 @@ extern "C"
 {
 #endif /* __cplusplus */
 
-/* Response and Event Callbacks */
-typedef struct {
-    /* Various Events Callback */
-    void (*on_link_stats_results) (wifi_request_id id,
-                                   wifi_iface_stat *iface_stat,
-                                   int num_radios,
-                                   wifi_radio_stat *radio_stat);
-} LLStatsCallbackHandler;
-
 typedef struct{
     u32 stats_clear_rsp_mask;
     u8 stop_rsp;
@@ -88,7 +79,7 @@ private:
 
     LLStatsResultsParams mResultsParams;
 
-    LLStatsCallbackHandler mHandler;
+    wifi_stats_result_handler mHandler;
 
     wifi_request_id mRequestId;
 
@@ -111,15 +102,12 @@ public:
 
     virtual int handleResponse(WifiEvent &reply);
 
-    virtual int handleEvent(WifiEvent &event);
-
-    virtual int setCallbackHandler(LLStatsCallbackHandler nHandler, u32 event);
-
-    virtual void unregisterHandler(u32 subCmd);
-
     virtual void getClearRspParams(u32 *stats_clear_rsp_mask, u8 *stop_rsp);
 
-    virtual int get_wifi_iface_stats(wifi_iface_stat *stats, struct nlattr **tb_vendor);
+    virtual wifi_error get_wifi_iface_stats(wifi_iface_stat *stats,
+                                            struct nlattr **tb_vendor);
+
+    virtual void setHandler(wifi_stats_result_handler handler);
 };
 
 #ifdef __cplusplus
