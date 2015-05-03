5db91c296a86d8fea44f5a3ca2c154a298c6f47e
beanstown106 <nbenis106@gmail.com>
2015-05-03 03:50:51 -0400

    Use Google Nexus Audio Files [1/2]
    Change-Id: I11528ac9c838ebef14c889f13f34dda458b2f7a4

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -39,14 +39,14 @@ PRODUCT_PACKAGES += \
 
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg
+    ro.config.ringtone=Titania.ogg \
+    ro.config.notification_sound=Tethys.ogg
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/GoogleAudio.mk)
 
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
