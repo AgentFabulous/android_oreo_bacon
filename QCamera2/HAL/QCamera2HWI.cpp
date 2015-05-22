fc345c2d5f6093bbf174b9b6b28bb871be42f213
Rajaram Gudivada <rajaramg@codeaurora.org>
2015-05-22 15:51:43 +0530

    QCamera2: HAL: Restart the preview channel
    Issue:
    In some of the 3rd party Application's, Torch mode is set
    in preview stopped state with default dimensions and
    also recording hint is set to false.
    so, we add a preview channel thinking it.s a flash case and
    then App is setting the actual preview dimensions which we
    don't honor since the preview channel is already there and
    due to this mismatch in dimensions, garbled preview is seen.
    
    Fix:
    Delete the Preview Channel if it.s already there and add a
    new preview channel with updated dimensions & format.
    
    Change-Id: Ia1bd0d30a7939b8bdfd5d88fe100f696dfb4a7c6

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -3943,11 +3943,9 @@ int32_t QCamera2HardwareInterface::addPreviewChannel()
     bool raw_yuv = false;
 
     if (m_channels[QCAMERA_CH_TYPE_PREVIEW] != NULL) {
-        // Using the no preview torch WA it is possible
-        // to already have a preview channel present before
-        // start preview gets called.
-        ALOGD(" %s : Preview Channel already added!", __func__);
-        return NO_ERROR;
+        ALOGD("%s : Preview Channel already added and so delete it", __func__);
+        delete m_channels[QCAMERA_CH_TYPE_PREVIEW];
+        m_channels[QCAMERA_CH_TYPE_PREVIEW] = NULL;
     }
 
     pChannel = new QCameraChannel(mCameraHandle->camera_handle,
