6f032ec796a9fa72c1703c93c41f5261d2080a59
Zachariah <zkennedy87@gmail.com>
2015-07-15 01:22:19 -0400

    cpufreq: Blu_Active: Initial Port for OPO
    Signed-off-by: Zachariah <zkennedy87@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -418,6 +418,9 @@ extern struct cpufreq_governor cpufreq_gov_nightmare;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_LIONHEART)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_blu_active)
 #endif
 
 
