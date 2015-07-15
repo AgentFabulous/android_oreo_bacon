fee2e0d8a289ea1ca9eaa9a4cf185329434ef90d
Steve Paik <spaik@google.com>
2015-07-15 16:50:00 -0700

    Workaround for adp8064
    This is a squash of Ic529e3f1c30b012bbb50001a8fbee2ff9455accc and
    I17ef0ca49128954a8f84cb7eacb27cf9c9e4dcb1
    
    Change-Id: I051c5f6683fc6161b5362cba456631a464cc8453

--- Android.mk
+++ Android.mk
@@ -1,7 +1,10 @@
-ifneq ($(filter msm8960 msm8x27 msm8974 msm8226,$(TARGET_BOARD_PLATFORM)),)
-include $(call all-named-subdir-makefiles,msm8960)
-else
-ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
-include $(call all-named-subdir-makefiles,msm8992)
-endif
+# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
+ifneq ($(TARGET_BOARD_AUTO),true)
+  ifneq ($(filter msm8960 msm8x27 msm8974 msm8226,$(TARGET_BOARD_PLATFORM)),)
+    include $(call all-named-subdir-makefiles,msm8960)
+  else
+    ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
+      include $(call all-named-subdir-makefiles,msm8992)
+    endif
+  endif
 endif
