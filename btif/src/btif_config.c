aa3be1a696b9ae1142fe9f358ad5f97e99da7892
Ajay Panicker <apanicke@google.com>
2015-07-28 16:54:53 -0700

    Implement Bluetooth settings factory reset (3/5)
    Implemented the factory reset function to be used to reset all bluetooth
    settings on device to factory default
    
    Bug: 16161518

--- btif/src/btif_config.c
+++ btif/src/btif_config.c
@@ -360,6 +360,26 @@ void btif_config_flush(void) {
   pthread_mutex_unlock(&lock);
 }
 
+int btif_config_clear(void){
+  assert(config != NULL);
+  assert(alarm_timer != NULL);
+
+  alarm_cancel(alarm_timer);
+
+  pthread_mutex_lock(&lock);
+  config_free(config);
+
+  config = config_new_empty();
+  if (config == NULL) {
+    pthread_mutex_unlock(&lock);
+    return false;
+  }
+
+  int ret = config_save(config, CONFIG_FILE_PATH);
+  pthread_mutex_unlock(&lock);
+  return ret;
+}
+
 static void timer_config_save(UNUSED_ATTR void *data) {
   assert(config != NULL);
   assert(alarm_timer != NULL);
