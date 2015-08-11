065cf9757cc41b98e9a0e9bf724dd1ca015b5d6d
Sultanxda <sultanxda@gmail.com>
2015-08-10 23:04:23 -0700

    QCamera2: Allow frontend to control focus mode for scenes
    *Fixes touch-to-focus in HDR mode

--- QCamera2/HAL/QCameraParameters.cpp
+++ QCamera2/HAL/QCameraParameters.cpp
@@ -1787,6 +1787,7 @@ int32_t QCameraParameters::setFocusMode(const QCameraParameters& params)
     const char *str = params.get(KEY_FOCUS_MODE);
     const char *prev_str = get(KEY_FOCUS_MODE);
 
+#if 0 // Allow frontend to control scene focus mode
     //Find whether scene mode is Auto or not. We should set focus mode set by app
     //only in Auto scene mode.  For other scene modes, Focus mode corresponding to
     //a scene is decided and set in backend. In HAL, it is taken care in setScenePreferences.
@@ -1794,10 +1795,15 @@ int32_t QCameraParameters::setFocusMode(const QCameraParameters& params)
     const char *scene_str = params.get(KEY_SCENE_MODE);
     if(!strcmp(scene_str, SCENE_MODE_AUTO))
         isAutoSceneMode = TRUE;
+#endif
 
     if (str != NULL) {
         if (prev_str == NULL ||
+#if 0 // Allow frontend to control scene focus mode
             (strcmp(str, prev_str) != 0 && isAutoSceneMode)){
+#else
+            (strcmp(str, prev_str) != 0)){
+#endif
                 rc = setFocusMode(str);
         }
     }
