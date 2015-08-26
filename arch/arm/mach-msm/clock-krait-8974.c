3ecf0df88120575f9bd395de1b379f492b5b4242
anarkia1976 <stefano.villa1976@gmail.com>
2015-08-26 23:46:21 +0200

    clock-krait-8974: Expose SPEED level via sysfs
    Conflicts:
    	arch/arm/mach-msm/clock-krait-8974.c

--- arch/arm/mach-msm/clock-krait-8974.c
+++ arch/arm/mach-msm/clock-krait-8974.c
@@ -35,6 +35,10 @@
 int pvs_level = -1;
 module_param(pvs_level, int, S_IRUGO); 
 #endif
+#ifdef CONFIG_SPEED_LEVEL_INTERFACE
+int speed_level = -1;
+module_param(speed_level, int, S_IRUGO);
+#endif
 
 /* Clock inputs coming into Krait subsystem */
 DEFINE_FIXED_DIV_CLK(hfpll_src_clk, 1, NULL);
@@ -492,6 +496,10 @@ static void get_krait_bin_format_b(struct platform_device *pdev,
 			devm_iounmap(&pdev->dev, base_svs);
 		}
 	}
+
+#ifdef CONFIG_SPEED_LEVEL_INTERFACE
+        speed_level = *speed;
+#endif
 
 	/* Check PVS_BLOW_STATUS */
 	pte_efuse = readl_relaxed(base + 0x4) & BIT(21);
