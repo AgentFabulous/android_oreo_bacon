6634b1aaa20eab016c89da44a2bfb06305dc4045
Sultanxda <sultanxda@gmail.com>
2015-09-11 22:45:48 -0700

    QCamera2: Add missing preview sizes for back camera
    Just to be safe. These resolutions are used in the original, unmodified preview-size table.

--- QCamera2/HAL/QCameraParameters.cpp
+++ QCamera2/HAL/QCameraParameters.cpp
@@ -4380,7 +4380,7 @@ static cam_dimension_t new_vid_sizes_cam0[CAM0_VID_TBL_SIZE] = {
     {160, 120}
 };
 
-#define CAM0_PRVW_TBL_SIZE 13
+#define CAM0_PRVW_TBL_SIZE 16
 static cam_dimension_t new_prvw_sizes_cam0[CAM0_PRVW_TBL_SIZE] = {
     {4096, 2160},
     {3840, 2160},
@@ -4389,10 +4389,13 @@ static cam_dimension_t new_prvw_sizes_cam0[CAM0_PRVW_TBL_SIZE] = {
     {1440, 1080},
     {1280, 960},
     {1280, 720},
+    {864, 480},
+    {800, 480},
     {768, 432},
     {720, 480},
     {640, 480},
     {576, 432},
+    {480, 320},
     {384, 288},
     {320, 240}
 };
