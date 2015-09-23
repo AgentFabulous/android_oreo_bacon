2990f79de941189cfcce32e8bd7dc9e75bc118a5
Pavlin Radoslavov <pavlin@google.com>
2015-09-23 10:08:20 -0700

    GKI cleanup - Eliminate function GKI_get_pool_bufsize()
    Change-Id: I87168157b9bb3e9d315c03b98a10d18b38cce3e1

--- gki/common/gki.h
+++ gki/common/gki.h
@@ -79,7 +79,6 @@ void    GKI_init_q (BUFFER_Q *);
 UINT16  GKI_queue_length(BUFFER_Q *);
 BOOLEAN GKI_queue_is_empty(BUFFER_Q *);
 void   *GKI_remove_from_queue (BUFFER_Q *, void *);
-UINT16  GKI_get_pool_bufsize (UINT8);
 
 /* Disable Interrupts, Enable Interrupts
 */
