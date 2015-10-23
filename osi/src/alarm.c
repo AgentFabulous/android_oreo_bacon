41a91a52b06171140938a1aa503bab8fe15e12e4
Andre Eisenbach <eisenbach@google.com>
2015-10-23 10:40:36 -0700

    Raise alarm callback thread priority
    Since alarms [/callbacks] are used in the critical audio path, raising
    the thread priority is required to ensure timely encoding/transmission
    of audio packets and avoiding thread pre-emption.
    
    This patch indiscriminatly raises the (single) callback thread priority,
    affecting all alarm callbacks. In the future, thread priotiry should be
    adjusted based on the alarms pending in the queue.
    
    Bug: 24570959
    Change-Id: I64928fd7325f1d6f76815a185dbccf1e01ae30d2

--- osi/src/alarm.c
+++ osi/src/alarm.c
@@ -39,6 +39,13 @@
 #include "osi/include/semaphore.h"
 #include "osi/include/thread.h"
 
+// Make callbacks run at high thread priority. Some callbacks are used for audio
+// related timer tasks as well as re-transmissions etc. Since we at this point
+// cannot differentiate what callback we are dealing with, assume high priority
+// for now.
+// TODO(eisenbach): Determine correct thread priority (from parent?/per alarm?)
+static const int CALLBACK_THREAD_PRIORITY_HIGH = -19;
+
 struct alarm_t {
   // The lock is held while the callback for this alarm is being executed.
   // It allows us to release the coarse-grained monitor lock while a potentially
@@ -262,6 +269,7 @@ static bool lazy_initialize(void) {
     goto error;
   }
 
+  thread_set_priority(callback_thread, CALLBACK_THREAD_PRIORITY_HIGH);
   thread_post(callback_thread, callback_dispatch, NULL);
   return true;
 
