592afafb911cccc8aaa7d74c54f638e6bfb0a62d
Pavlin Radoslavov <pavlin@google.com>
2015-11-25 00:06:23 -0800

    Add missing initialization of module OSI_MODULE
    Also, fix a resource leak (memory + file descriptors)
    inside function AVDT_WriteReqOpt().
    The leak was exposed by the module OSI_MODULE initialization fix
    when audio is playing.
    
    Bug: 21743601
    Change-Id: Ia220042b9060b0640ac0851f4f9132ddf2773264

--- btcore/src/osi_module.c
+++ btcore/src/osi_module.c
@@ -37,7 +37,7 @@ future_t *osi_clean_up(void) {
   return future_new_immediate(FUTURE_SUCCESS);
 }
 
-const module_t osi_module = {
+EXPORT_SYMBOL const module_t osi_module = {
   .name = OSI_MODULE,
   .init = osi_init,
   .start_up = NULL,
