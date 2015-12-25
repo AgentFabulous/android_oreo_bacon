e4df4bf78689f9f3ce3dcefb30fd0d312b59f885
franciscofranco <franciscofranco.1990@gmail.com>
2015-12-25 17:54:33 +0000

    cpufreq: cpu-boost: export input_boost_enable to userspace
    Users should have the choice to run touchboost-less (even though it ruins performance)
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/cpu-boost.c

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -54,7 +54,8 @@ module_param(boost_ms, uint, 0644);
 static unsigned int sync_threshold;
 module_param(sync_threshold, uint, 0644);
 
-static bool input_boost_enabled;
+static unsigned int input_boost_enabled = 1;
+module_param(input_boost_enabled, uint, 0644);
 static bool suspended;
 
 static unsigned int input_boost_ms = 40;
@@ -78,7 +79,6 @@ static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
 	int i, ntokens = 0;
 	unsigned int val, cpu;
 	const char *cp = buf;
-	bool enabled = false;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
@@ -89,7 +89,7 @@ static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
 			return -EINVAL;
 		for_each_possible_cpu(i)
 			per_cpu(sync_info, i).input_boost_freq = val;
-		goto check_enable;
+		goto out;
 	}
 
 	/* CPU:value pair */
@@ -108,15 +108,7 @@ static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
 		cp++;
 	}
 
-check_enable:
-	for_each_possible_cpu(i) {
-		if (per_cpu(sync_info, i).input_boost_freq) {
-			enabled = true;
-			break;
-		}
-	}
-	input_boost_enabled = enabled;
-
+out:
 	return 0;
 }
 
