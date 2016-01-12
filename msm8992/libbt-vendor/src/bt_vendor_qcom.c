62a4057b65f11c31e209a06e735986de01d14be3
Sunny Kapdi <sunnyk@codeaurora.org>
2016-01-11 18:39:54 -0800

    Delay UART CLK_OFF during BT init sequence
    UART clk vote was not held for a short duration post
    firmware-download sequence and prior to returning from
    vendor open callback. Since libbt-vendor relies on UART
    to hold the wakelock, and the stack/framework doesn't hold
    a wakelock during BT init, there is a chance that the system
    can go to sleep during BT_VND_OP_USERIAL_OPEN without this fix.
    
    Bug: 24669317
    Change-Id: Idd568dd70c8ae82a548dae8a3d59199e24e031e9

--- msm8992/libbt-vendor/src/bt_vendor_qcom.c
+++ msm8992/libbt-vendor/src/bt_vendor_qcom.c
@@ -701,7 +701,7 @@ static int op(bt_vendor_opcode_t opcode, void *param)
         case BT_VND_OP_USERIAL_OPEN:
             {
                 int (*fd_array)[] = (int (*)[]) param;
-                int idx, fd;
+                int fd = -1, fd_filter = -1;
                 ALOGI("bt-vendor : BT_VND_OP_USERIAL_OPEN");
                 switch(btSocType)
                 {
@@ -724,11 +724,12 @@ static int op(bt_vendor_opcode_t opcode, void *param)
                         break;
                     case BT_SOC_AR3K:
                         {
+                            int idx;
                             fd = userial_vendor_open((tUSERIAL_CFG *) &userial_init_cfg);
                             if (fd != -1) {
                                 for (idx=0; idx < CH_MAX; idx++)
                                     (*fd_array)[idx] = fd;
-                                     retval = 1;
+                                retval = 1;
                             }
                             else {
                                 retval = -1;
@@ -745,6 +746,7 @@ static int op(bt_vendor_opcode_t opcode, void *param)
                         break;
                     case BT_SOC_ROME:
                         {
+                            int idx;
                             property_get("persist.BT3_2.version", bt_version, false);
                             if (!is_soc_initialized()) {
                                 fd = userial_vendor_open((tUSERIAL_CFG *) &userial_init_cfg);
@@ -810,13 +812,9 @@ static int op(bt_vendor_opcode_t opcode, void *param)
 #endif //BT_NV_SUPPORT
                                     if(rome_soc_init(fd,vnd_local_bd_addr)<0) {
                                         retval = -1;
-                                        userial_clock_operation(fd, USERIAL_OP_CLK_OFF);
                                     } else {
                                         ALOGV("rome_soc_init is completed");
                                         property_set("wc_transport.soc_initialized", "1");
-                                        userial_clock_operation(fd, USERIAL_OP_CLK_OFF);
-                                        /*Close the UART port*/
-                                        close(fd);
                                     }
                                 }
                             }
@@ -827,35 +825,37 @@ static int op(bt_vendor_opcode_t opcode, void *param)
                                  start_hci_filter();
                                  if (is_ant_req) {
                                      ALOGV("connect to ant channel");
-                                     ant_fd = fd = connect_to_local_socket("ant_sock");
+                                     ant_fd = fd_filter = connect_to_local_socket("ant_sock");
                                  }
                                  else
 #endif
                                  {
                                      ALOGV("connect to bt channel");
-                                     vnd_userial.fd = fd = connect_to_local_socket("bt_sock");
+                                     vnd_userial.fd = fd_filter = connect_to_local_socket("bt_sock");
                                  }
 
-                                 if (fd != -1) {
+                                 if (fd_filter != -1) {
                                      ALOGV("%s: received the socket fd: %d is_ant_req: %d\n",
-                                                                 __func__, fd, is_ant_req);
+                                                                 __func__, fd_filter, is_ant_req);
                                      if((strcmp(bt_version, "true") == 0) && !is_ant_req) {
                                          if (rome_ver >= ROME_VER_3_0) {
                                              /*  get rome supported feature request */
                                              ALOGE("%s: %x08 %0x", __FUNCTION__,rome_ver, ROME_VER_3_0);
-                                             rome_get_addon_feature_list(fd);
+                                             rome_get_addon_feature_list(fd_filter);
                                          }
                                      }
                                      for (idx=0; idx < CH_MAX; idx++)
-                                          (*fd_array)[idx] = fd;
-                                          retval = 1;
-                                     }
+                                         (*fd_array)[idx] = fd_filter;
+                                     retval = 1;
+                                 }
                                  else {
                                      retval = -1;
                                  }
-                             } else {
-                               if (fd >= 0)
-                                  close(fd);
+                             }
+
+                             if (fd >= 0) {
+                                 userial_clock_operation(fd, USERIAL_OP_CLK_OFF);
+                                 close(fd);
                              }
                         }
                         break;
