0b1e548e1b9fcd11b01e69ae9462b3815bd19fa8
Andre Eisenbach <eisenbach@google.com>
2016-01-14 02:00:24 -0800

    DO NOT MERGE Add ability to add interop entries dynamically (2/2)
    Add ability and interface for adding dynamic entries to the interop
    workaround database.
    
    Bug: 26548845
    Change-Id: Id886e4233fc1548727e79f1581cdc0c6f0738d59

--- main/bte_main.c
+++ main/bte_main.c
@@ -45,6 +45,9 @@
 #include "bt_utils.h"
 #include "btcore/include/counter.h"
 #include "btcore/include/module.h"
+#include "device/include/interop.h"
+#include "hci_layer.h"
+#include "osi/include/alarm.h"
 #include "osi/include/fixed_queue.h"
 #include "osi/include/future.h"
 #include "gki.h"
@@ -97,6 +100,7 @@ void bte_main_boot_entry(void)
 {
     module_init(get_module(GKI_MODULE));
     module_init(get_module(COUNTER_MODULE));
+    module_init(get_module(INTEROP_MODULE));
 
     hci = hci_layer_get_interface();
     if (!hci)
@@ -136,6 +140,7 @@ void bte_main_shutdown()
 
     module_clean_up(get_module(STACK_CONFIG_MODULE));
 
+    module_clean_up(get_module(INTEROP_MODULE));
     module_clean_up(get_module(COUNTER_MODULE));
     module_clean_up(get_module(GKI_MODULE));
 }
