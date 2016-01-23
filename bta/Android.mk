ad46ee235fa0828ebf4042d84f646424ffeadc31
Sharvil Nanavati <sharvil@google.com>
2016-01-22 16:54:33 -0800

    Remove unused definition BOARD_HAVE_BLUETOOTH_BCM.
    Change-Id: I4f47ffbec067ad1d7b4db0269cd5f7ee21ae7861

--- bta/Android.mk
+++ bta/Android.mk
@@ -2,10 +2,6 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ifeq ($(BOARD_HAVE_BLUETOOTH_BCM),true)
-LOCAL_CFLAGS += \
-	-DBOARD_HAVE_BLUETOOTH_BCM
-endif
 LOCAL_CFLAGS += -DBUILDCFG $(bdroid_CFLAGS) -std=c99
 LOCAL_CLANG_CFLAGS += -Wno-error=gnu-variable-sized-type-not-at-end
 
