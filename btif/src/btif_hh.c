fecf606348a0751e0be30e376c9817195afe68ca
Ajay Panicker <apanicke@google.com>
2016-03-10 15:30:16 -0800

    Add pair event device class and fix timestamp
    Added a function to get the device class to use with pair events.
    Also update check_cod_hid as it was only being used with COD_HID_MAJOR.
    Also fixed an issue with pair event timestamps where they were
    using the wrong clock.
    
    Bug: 27499938
    Change-Id: I0805d7a3f82b28e7d7e73d80818948843f51a82b

--- btif/src/btif_hh.c
+++ btif/src/btif_hh.c
@@ -50,7 +50,6 @@
 #define COD_HID_KEYBOARD        0x0540
 #define COD_HID_POINTING        0x0580
 #define COD_HID_COMBO           0x05C0
-#define COD_HID_MAJOR           0x0500
 
 #define KEYSTATE_FILEPATH "/data/misc/bluedroid/bt_hh_ks" //keep this in sync with HID host jni
 
@@ -149,7 +148,7 @@ extern void bta_hh_co_send_hid_info(btif_hh_device_t *p_dev, char *dev_name, UIN
                                     int dscp_len, UINT8 *p_dscp);
 extern BOOLEAN check_cod(const bt_bdaddr_t *remote_bdaddr, uint32_t cod);
 extern void btif_dm_cb_remove_bond(bt_bdaddr_t *bd_addr);
-extern BOOLEAN check_cod_hid(const bt_bdaddr_t *remote_bdaddr, uint32_t cod);
+extern BOOLEAN check_cod_hid(const bt_bdaddr_t *remote_bdaddr);
 extern int  scru_ascii_2_hex(char *p_ascii, int len, UINT8 *p_hex);
 extern void btif_dm_hh_open_failed(bt_bdaddr_t *bdaddr);
 
@@ -1021,7 +1020,7 @@ static void btif_hh_upstreams_evt(UINT16 event, char* p_param)
                     BTIF_TRACE_DEBUG("%s---Removing HID bond", __FUNCTION__);
                     /* If it is locally initiated VUP or remote device has its major COD as
                     Peripheral removed the bond.*/
-                    if (p_dev->local_vup  || check_cod_hid(&(p_dev->bd_addr), COD_HID_MAJOR))
+                    if (p_dev->local_vup  || check_cod_hid(&(p_dev->bd_addr)))
                     {
                         p_dev->local_vup = FALSE;
                         BTA_DmRemoveDevice((UINT8 *)p_dev->bd_addr.address);
