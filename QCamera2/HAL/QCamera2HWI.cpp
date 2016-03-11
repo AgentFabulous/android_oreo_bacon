a369ca83222192ab05d9f24dc7da66ada94acdb3
Sultanxda <sultanxda@gmail.com>
2016-03-11 10:42:37 -0800

    QCamera2: Re-order HDR exposure sequence
    Set the last value to 0 so the viewfinder has normal exposure after HDR is
    finished.

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -1435,6 +1435,11 @@ int QCamera2HardwareInterface::initCapabilities(int cameraId,mm_camera_vtbl_t *c
     memcpy(gCamCapability[cameraId], DATA_PTR(capabilityHeap,0),
                                         sizeof(cam_capability_t));
 
+    // Re-order HDR exposure sequence to prevent screen flash on last frame
+    gCamCapability[cameraId]->hdr_bracketing_setting.exp_val.values[0] = -6;
+    gCamCapability[cameraId]->hdr_bracketing_setting.exp_val.values[1] = 6;
+    gCamCapability[cameraId]->hdr_bracketing_setting.exp_val.values[2] = 0;
+
     // Inject modified video/preview size tables
     if (gCamCapability[cameraId]->position == CAM_POSITION_BACK) {
         for (i = 0; i < CAM0_PIC_TBL_SIZE; i++)
