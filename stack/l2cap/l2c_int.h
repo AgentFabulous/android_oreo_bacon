494b510b9bdf5bf9bb8b74181a8f640e54e6cd40
Gurpreet Ghai <gghai@codeaurora.org>
2016-03-13 21:48:30 +0530

    Check CCB state before executing channel state machine events
    Use Case:
    When a disconnect response is being executed, a timeout may
    occur and timeout event gets enqueued in BTU process. When
    processing of the disconnect event completes, timout event is received
    at csm and is executed. However, by this time the Channel
    Control Block is already released during previous event causing
    errors.
    
    Steps:
    Various connection/disconnection scenario.
    
    Failure:
    If ccb is already released while processing of
    previous event, the occurance of new enqueued event causes
    crash.
    
    Root cause:
    Disconnection response event has already released
    ccb and set lcb for this channel to null. The occurance of
    timer event after this results in crash because lcb is
    dereferenced while header creation to send disc response to
    peer device.
    
    Fix:
    Added check for ccb state for whether it is currently in
    use or released before executing events in csm.
    
    Change-Id: I9110e6dd5273fa162b51c8aa15bd0030567d664b

--- stack/l2cap/l2c_int.h
+++ stack/l2cap/l2c_int.h
@@ -639,6 +639,7 @@ extern void l2cu_send_peer_ble_credit_based_disconn_req(tL2C_CCB *p_ccb);
 extern BOOLEAN l2cu_initialize_fixed_ccb (tL2C_LCB *p_lcb, UINT16 fixed_cid, tL2CAP_FCR_OPTS *p_fcr);
 extern void    l2cu_no_dynamic_ccbs (tL2C_LCB *p_lcb);
 extern void    l2cu_process_fixed_chnl_resp (tL2C_LCB *p_lcb);
+extern BOOLEAN l2cu_is_ccb_active (tL2C_CCB *p_ccb);
 
 /* Functions provided by l2c_ucd.c
 ************************************
