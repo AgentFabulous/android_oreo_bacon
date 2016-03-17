b9d5c2d3ba13bce028d3abcae8a9ddd23eb6b553
Pierre Imai <imaipi@google.com>
2016-03-17 14:41:30 +0900

    Remove references to deleted dhcpcd
    Change-Id: Ic5c79807e6bfd0e507ad9a7f1a14f73a16acb50c

--- qcwcn/config/Android.mk
+++ qcwcn/config/Android.mk
@@ -15,15 +15,6 @@
 #
 LOCAL_PATH := $(call my-dir)
 
-########################
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := dhcpcd.conf
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/dhcpcd
-LOCAL_SRC_FILES := android_dhcpcd.conf
-include $(BUILD_PREBUILT)
-
 #########################
 
 WIFI_DRIVER_SOCKET_IFACE := wlan0
