e0515357ad82e7a86b9b5d0fe2e187ef74f940b8
Sultanxda <sultanxda@gmail.com>
2016-03-20 14:18:37 -0700

    Snap: Always enable developer menu

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -381,7 +381,8 @@ public class CameraActivity extends Activity
     }
 
     public boolean isDeveloperMenuEnabled() {
-        return mDeveloperMenuEnabled;
+        //return mDeveloperMenuEnabled;
+        return true;
     }
 
     public void enableDeveloperMenu() {
