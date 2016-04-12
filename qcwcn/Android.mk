aa37e600d36332e9ec9019f3d34c572cef02d3a5
Steve Paik <spaik@google.com>
2016-04-12 11:53:32 -0700

    Share Wifi driver with bat project
    - Remove only adp8064 and fox builds from using common wifi folder.
    
    Change-Id: I5674405d50d24c85af4a4ca545213e11679ff54b
    (cherry picked from commit 2a7a1870c94470e203baf0eb6f569daba76ea71f)

--- qcwcn/Android.mk
+++ qcwcn/Android.mk
@@ -1,4 +1,5 @@
-ifneq ($(TARGET_BOARD_AUTO),true)
+# adp8064 and fox box do not share wifi code
+ifeq ($(filter adp8064 fox,$(TARGET_DEVICE)),)
   ifeq ($(BOARD_WLAN_DEVICE),qcwcn)
     include $(call all-subdir-makefiles)
   endif
