AgentFabulous/android_oreo_bacon

commit 3a2c2d61f4c8769ea9c1615208a6daf2fa162084

Retry HID Polling when EINTR is returned
While I'm here
 - use __func__ consistently in btif_hh_poll_event_thread.
 - remove braces around a single line

BUG: 24647083
Change-Id: I80b99ff0d3ba6336f5ab7a517269fdf63df59b37
Signed-off-by: Myles Watson <mylesgw@google.com>

btif/co/bta_hh_co.c
@@ -195,7 +195,7 @@ static inline pthread_t create_thread(void *(*start_routine)(void *), void * arg
 static void *btif_hh_poll_event_thread(void *arg)
 {
     btif_hh_device_t *p_dev = arg;
-    APPL_TRACE_DEBUG("%s: Thread created fd = %d", __FUNCTION__, p_dev->fd);
+    APPL_TRACE_DEBUG("%s: Thread created fd = %d", __func__, p_dev->fd);
     struct pollfd pfds[1];
     int ret;
 
@@ -208,15 +208,14 @@ static void *btif_hh_poll_event_thread(void *arg)
     while(p_dev->hh_keep_polling){
         ret = poll(pfds, 1, 50);
         if (ret < 0) {
-            APPL_TRACE_ERROR("%s: Cannot poll for fds: %s\n", __FUNCTION__, strerror(errno));
+            APPL_TRACE_ERROR("%s: Cannot poll for fds: %s\n", __func__, strerror(errno));
             break;
         }
         if (pfds[0].revents & POLLIN) {
-            APPL_TRACE_DEBUG("btif_hh_poll_event_thread: POLLIN");
+            APPL_TRACE_DEBUG("%s: POLLIN", __func__);
             ret = uhid_event(p_dev);
-            if (ret){
+            if (ret != -EINTR)
                 break;
-            }
         }
     }
 

commit 052679c0e9280b93d9695043f8379e6a0b81a37e

HID: Fix regression caused by commit "3a2c2d61"
uhid_event function returned 0 in normal case and with below
commit polling thread was being exited resulting in user space
not able to read the events sent by uhid kernel driver leading
to IOT issues with HID devices using the vendor hid usb drivers.
Eg: Apple Magic Mouse scroll functionality was not working as
for normal functionality of Apple Magic mouse, output report
needs to be sent to hid device, which was not being sent with
current implemenation.

commit 3a2c2d61f4c8769ea9c1615208a6daf2fa162084
Author: Myles Watson <mylesgw@google.com>
Date:   Tue Nov 10 14:35:57 2015 -0800

    Retry HID Polling when EINTR is returned

    While I'm here
     - use __func__ consistently in btif_hh_poll_event_thread.
     - remove braces around a single line

    BUG: 24647083
    Change-Id: I80b99ff0d3ba6336f5ab7a517269fdf63df59b37
    Signed-off-by: Myles Watson <mylesgw@google.com>

Change-Id: If1f69c8d024fd7d03e2d79f19b962653fe2dd8b9

btif/co/bta_hh_co.c
@@ -215,7 +215,7 @@ static void *btif_hh_poll_event_thread(void *arg)
         if (pfds[0].revents & POLLIN) {
             APPL_TRACE_DEBUG("%s: POLLIN", __func__);
             ret = uhid_event(p_dev);
-            if (ret != -EINTR)
+            if (ret != -EINTR && ret != 0)
                 break;
         }
     }