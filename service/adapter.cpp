GrapheneOS/platform_packages_modules_Bluetooth

Add guest mode functionality (2/5)
Add a flag to enable() to start Bluetooth in restricted
mode. In restricted mode, all devices that are paired during
restricted mode are deleted upon leaving restricted mode.
Right now restricted mode is only entered while a guest
user is active.

Bug: 27410683
Change-Id: I19ff91d1d7fb8c60230fee7a05d32574b5b2aeda

system/service/adapter.cpp
@@ -105,7 +105,7 @@ class AdapterImpl : public Adapter,
     return state_.load() == ADAPTER_STATE_ON;
   }
 
-  bool Enable() override {
+  bool Enable(bool startRestricted) override {
     AdapterState current_state = GetState();
     if (current_state != ADAPTER_STATE_OFF) {
       LOG(INFO) << "Adapter not disabled - state: "
@@ -118,7 +118,7 @@ class AdapterImpl : public Adapter,
     state_ = ADAPTER_STATE_TURNING_ON;
     NotifyAdapterStateChanged(current_state, state_);
 
-    int status = hal::BluetoothInterface::Get()->GetHALInterface()->enable();
+    int status = hal::BluetoothInterface::Get()->GetHALInterface()->enable(startRestricted);
     if (status != BT_STATUS_SUCCESS) {
       LOG(ERROR) << "Failed to enable Bluetooth - status: "
                  << BtStatusText((const bt_status_t)status);

system/service/common/bluetooth/binder/IBluetooth.cpp
@@ -81,7 +81,8 @@ status_t BnBluetooth::onTransact(
     }
     case ENABLE_TRANSACTION: {
       CHECK_INTERFACE(IBluetooth, data, reply);
-      bool result = Enable();
+      bool startRestricted = data.readBool();
+      bool result = Enable(startRestricted);
       reply->writeInt32(result);
       return android::NO_ERROR;
     }
@@ -182,10 +183,11 @@ int BpBluetooth::GetState() {
   return reply.readInt32();
 }
 
-bool BpBluetooth::Enable() {
+bool BpBluetooth::Enable(bool startRestricted) {
   Parcel data, reply;
 
   data.writeInterfaceToken(IBluetooth::getInterfaceDescriptor());
+  data.writeBool(startRestricted);
   remote()->transact(IBluetooth::ENABLE_TRANSACTION, data, &reply);
 
   return reply.readInt32();

system/service/common/bluetooth/binder/IBluetooth.h
@@ -134,7 +134,7 @@ class IBluetooth : public android::IInterface {
 
   virtual bool IsEnabled() = 0;
   virtual int GetState() = 0;
-  virtual bool Enable() = 0;
+  virtual bool Enable(bool startRestricted) = 0;
   virtual bool EnableNoAutoConnect() = 0;
   virtual bool Disable() = 0;
 
@@ -184,7 +184,7 @@ class BpBluetooth : public android::BpInterface<IBluetooth> {
   // IBluetooth overrides:
   bool IsEnabled() override;
   int GetState() override;
-  bool Enable() override;
+  bool Enable(bool startRestricted) override;
   bool EnableNoAutoConnect() override;
   bool Disable() override;
 

system/service/ipc/binder/bluetooth_binder_server.cpp
@@ -50,9 +50,9 @@ int BluetoothBinderServer::GetState() {
   return adapter_->GetState();
 }
 
-bool BluetoothBinderServer::Enable() {
+bool BluetoothBinderServer::Enable(bool startRestricted) {
   VLOG(2) << __func__;
-  return adapter_->Enable();
+  return adapter_->Enable(startRestricted);
 }
 
 bool BluetoothBinderServer::EnableNoAutoConnect() {

system/service/ipc/binder/bluetooth_binder_server.h
@@ -45,7 +45,7 @@ class BluetoothBinderServer : public BnBluetooth,
   // IBluetooth overrides:
   bool IsEnabled() override;
   int GetState() override;
-  bool Enable() override;
+  bool Enable(bool startRestricted) override;
   bool EnableNoAutoConnect() override;
   bool Disable() override;
 

AgentFabulous/android_oreo_bacon

Change-Id: I8f23d28ef0aa3a8df13d469c73005c8e1b894d19

service/adapter.cpp
@@ -105,7 +105,7 @@ class AdapterImpl : public Adapter,
     return state_.load() == ADAPTER_STATE_ON;
   }
 
-  bool Enable() override {
+  bool Enable(bool start_restricted) override {
     AdapterState current_state = GetState();
     if (current_state != ADAPTER_STATE_OFF) {
       LOG(INFO) << "Adapter not disabled - state: "
@@ -118,7 +118,7 @@ class AdapterImpl : public Adapter,
     state_ = ADAPTER_STATE_TURNING_ON;
     NotifyAdapterStateChanged(current_state, state_);
 
-    int status = hal::BluetoothInterface::Get()->GetHALInterface()->enable();
+    int status = hal::BluetoothInterface::Get()->GetHALInterface()->enable(start_restricted);
     if (status != BT_STATUS_SUCCESS) {
       LOG(ERROR) << "Failed to enable Bluetooth - status: "
                  << BtStatusText((const bt_status_t)status);

service/common/bluetooth/binder/IBluetooth.cpp
@@ -81,7 +81,8 @@ status_t BnBluetooth::onTransact(
     }
     case ENABLE_TRANSACTION: {
       CHECK_INTERFACE(IBluetooth, data, reply);
-      bool result = Enable();
+      bool start_restricted = data.readBool();
+      bool result = Enable(start_restricted);
       reply->writeInt32(result);
       return android::NO_ERROR;
     }
@@ -182,10 +183,11 @@ int BpBluetooth::GetState() {
   return reply.readInt32();
 }
 
-bool BpBluetooth::Enable() {
+bool BpBluetooth::Enable(bool start_restricted) {
   Parcel data, reply;
 
   data.writeInterfaceToken(IBluetooth::getInterfaceDescriptor());
+  data.writeBool(start_restricted);
   remote()->transact(IBluetooth::ENABLE_TRANSACTION, data, &reply);
 
   return reply.readInt32();

service/common/bluetooth/binder/IBluetooth.h
@@ -134,7 +134,7 @@ class IBluetooth : public android::IInterface {
 
   virtual bool IsEnabled() = 0;
   virtual int GetState() = 0;
-  virtual bool Enable() = 0;
+  virtual bool Enable(bool start_restricted) = 0;
   virtual bool EnableNoAutoConnect() = 0;
   virtual bool Disable() = 0;
 
@@ -184,7 +184,7 @@ class BpBluetooth : public android::BpInterface<IBluetooth> {
   // IBluetooth overrides:
   bool IsEnabled() override;
   int GetState() override;
-  bool Enable() override;
+  bool Enable(bool start_restricted) override;
   bool EnableNoAutoConnect() override;
   bool Disable() override;
 

service/ipc/binder/bluetooth_binder_server.cpp
@@ -50,9 +50,9 @@ int BluetoothBinderServer::GetState() {
   return adapter_->GetState();
 }
 
-bool BluetoothBinderServer::Enable() {
+bool BluetoothBinderServer::Enable(bool start_restricted) {
   VLOG(2) << __func__;
-  return adapter_->Enable();
+  return adapter_->Enable(start_restricted);
 }
 
 bool BluetoothBinderServer::EnableNoAutoConnect() {

service/ipc/binder/bluetooth_binder_server.h
@@ -45,7 +45,7 @@ class BluetoothBinderServer : public BnBluetooth,
   // IBluetooth overrides:
   bool IsEnabled() override;
   int GetState() override;
-  bool Enable() override;
+  bool Enable(bool start_restricted) override;
   bool EnableNoAutoConnect() override;
   bool Disable() override;
 
@@ -65,7 +65,6 @@ class BluetoothBinderServer : public BnBluetooth,
   android::sp<IBluetoothGattServer> GetGattServerInterface() override;
 
   android::status_t dump(int fd, const android::Vector<android::String16>& args) override;
-
   // bluetooth::Adapter::Observer overrides:
   void OnAdapterStateChanged(bluetooth::Adapter* adapter,
                              bluetooth::AdapterState prev_state,