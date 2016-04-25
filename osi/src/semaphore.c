97ec0a56f5282ee8977bc1dcb60bb86980e757d6
Pavlin Radoslavov <pavlin@google.com>
2016-04-25 12:29:29 -0700

    Fix a race condition during alarm_cleanup()
    Fix a race condition inside function alarm_cleanup() that
    could be triggered during shutdown.
    
    Also, fix few other issues:
     * Add missing "timer_delete(akeup_timer)" statement inside
       alarm_cleanup()
     * Fix the argument when calling "timer_delete(timer)"
     * Call "semaphore_free(alarm_expired)" inside alarm_cleanup()
       after the corresponding "timer" and "wakeup_timer" have
       been deleted.
     * Fix the argument type when calling eventfd_read() inside
       semaphore_wait()
    
    Bug: 26982349
    Change-Id: I2b00cd7ee7f56f755775f8e7b370006e31c6eb08

--- osi/src/semaphore.c
+++ osi/src/semaphore.c
@@ -64,7 +64,7 @@ void semaphore_wait(semaphore_t *semaphore) {
   assert(semaphore != NULL);
   assert(semaphore->fd != INVALID_FD);
 
-  uint64_t value;
+  eventfd_t value;
   if (eventfd_read(semaphore->fd, &value) == -1)
     LOG_ERROR(LOG_TAG, "%s unable to wait on semaphore: %s", __func__, strerror(errno));
 }
