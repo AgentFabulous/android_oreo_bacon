002c6a19c55a6a22b78638b140c8d1cee33a0e87
Joe Maples <joe@frap129.org>
2016-07-16 19:28:52 +0000

    block: maple: Remove CONFIG_HZ dependency, assume 300 for interrupt frequency
    300 is the default for angler, so assume that while removing the dependency for finer tuning later on
    
    Signed-off-by: Joe Maples <joe@frap129.org>

--- block/maple-iosched.c
+++ block/maple-iosched.c
@@ -21,16 +21,14 @@
 
 #define MAPLE_IOSCHED_PATCHLEVEL	(7)
 
-enum { ASYNC, SYNC };
-
 /* Tunables */
-static const int sync_read_expire = HZ/3;	/* max time before a read sync is submitted. */
-static const int sync_write_expire = 7*HZ/6;	/* max time before a write sync is submitted. */
-static const int async_read_expire = 2*HZ/3;	/* ditto for read async, these limits are SOFT! */
-static const int async_write_expire = 5*HZ/3;	/* ditto for write async, these limits are SOFT! */
+static const int sync_read_expire = 100;	/* max time before a read sync is submitted. */
+static const int sync_write_expire = 350;	/* max time before a write sync is submitted. */
+static const int async_read_expire = 200;	/* ditto for read async, these limits are SOFT! */
+static const int async_write_expire = 500;	/* ditto for write async, these limits are SOFT! */
 static const int fifo_batch = 16;		/* # of sequential requests treated as one by the above parameters. */
 static const int writes_starved = 3;		/* max times reads can starve a write */
-static const int sleep_latency_multiple = 5;	/* multple for expire time when device is asleep */
+static const int sleep_latency_multiple = 5; /* multple for expire time when device is asleep */
 
 /* Elevator data */
 struct maple_data {
