c0209f7db1dbdc3032de1b478803a72fe93b4240
nikhil18 <nikhilgolechha1@gmail.com>
2016-08-09 18:25:17 +0530

    Revert "Voltage Control: generic voltage control for DTS based kernels"
    This reverts commit 77039ab1906a14d92f59072ac984e6f1243e944e.
    
    Conflicts:
    	arch/arm/mach-msm/Kconfig
    
    Conflicts:
    	arch/arm/mach-msm/Kconfig

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -33,10 +33,6 @@
 #include <linux/of.h>
 #include <trace/events/power.h>
 
-#ifdef CONFIG_CPU_VOLTAGE_TABLE
-static struct cpufreq_frequency_table *dts_freq_table;
-#endif
-
 static DEFINE_MUTEX(l2bw_lock);
 
 static struct clk *cpu_clk[NR_CPUS];
@@ -358,7 +354,7 @@ static struct cpufreq_driver msm_cpufreq_driver = {
 static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 						char *tbl_name, int cpu)
 {
-	int ret, nf, i, j;
+	int ret, nf, i;
 	u32 *data;
 	struct cpufreq_frequency_table *ftbl;
 
@@ -415,40 +411,10 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 	ftbl[i].index = i;
 	ftbl[i].frequency = CPUFREQ_TABLE_END;
 
-#ifdef CONFIG_CPU_VOLTAGE_TABLE
-	dts_freq_table =
-		devm_kzalloc(dev, (nf + 1) *
-			sizeof(struct cpufreq_frequency_table),
-			GFP_KERNEL);
-
-	if (!dts_freq_table)
-		return ERR_PTR(-ENOMEM);
-
-	for (i = 0, j = 0; i < nf; i++, j += 3)
-		dts_freq_table[i].frequency = data[j];
-	dts_freq_table[i].frequency = CPUFREQ_TABLE_END;
-#endif
-
 	devm_kfree(dev, data);
 
 	return ftbl;
 }
-
-#ifdef CONFIG_CPU_VOLTAGE_TABLE
-bool is_used_by_scaling(unsigned int freq)
-{
-	unsigned int i, cpu_freq;
-
-	for (i = 0; dts_freq_table[i].frequency != CPUFREQ_TABLE_END; i++) {
-		cpu_freq = dts_freq_table[i].frequency;
-		if (cpu_freq == CPUFREQ_ENTRY_INVALID)
-			continue;
-		if (freq == cpu_freq)
-			return true;
-	}
-	return false;
-}
-#endif
 
 static int __init msm_cpufreq_probe(struct platform_device *pdev)
 {
