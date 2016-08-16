GrapheneOS/platform_packages_modules_Bluetooth

Disable 3Mbps packets for AVDT ACL links
Change-Id: I4b1b52ec8aeaf5eab0672231889e0ea6f23aa852

system/stack/avdt/avdt_l2c.c
@@ -33,6 +33,7 @@
 #include "l2cdefs.h"
 #include "btm_api.h"
 #include "btm_int.h"
+#include "device/include/interop.h"
 
 
 /* callback function declarations */
@@ -202,6 +203,15 @@ void avdt_l2c_connect_ind_cback(BD_ADDR bd_addr, UINT16 lcid, UINT16 psm, UINT8 
             p_tbl->state = AVDT_AD_ST_SEC_ACP;
             p_tbl->cfg_flags = AVDT_L2C_CFG_CONN_ACP;
 
+            if (interop_match(INTEROP_2MBPS_LINK_ONLY, (const bt_bdaddr_t *)&bd_addr)) {
+                // Disable 3DH packets for AVDT ACL to improve sensitivity on HS
+                tACL_CONN *p_acl_cb = btm_bda_to_acl(bd_addr, BT_TRANSPORT_BR_EDR);
+                btm_set_packet_types(p_acl_cb, (btm_cb.btm_acl_pkt_types_supported |
+                                                HCI_PKT_TYPES_MASK_NO_3_DH1 |
+                                                HCI_PKT_TYPES_MASK_NO_3_DH3 |
+                                                HCI_PKT_TYPES_MASK_NO_3_DH5));
+            }
+
             /* Check the security */
             rc = btm_sec_mx_access_request (bd_addr, AVDT_PSM,
                 FALSE, BTM_SEC_PROTO_AVDT,
@@ -320,6 +330,15 @@ void avdt_l2c_connect_cfm_cback(UINT16 lcid, UINT16 result)
                         p_tbl->state = AVDT_AD_ST_SEC_INT;
                         p_tbl->lcid = lcid;
                         p_tbl->cfg_flags = AVDT_L2C_CFG_CONN_INT;
+
+                        if (interop_match(INTEROP_2MBPS_LINK_ONLY, (const bt_bdaddr_t *) &p_ccb->peer_addr)) {
+                            // Disable 3DH packets for AVDT ACL to improve sensitivity on HS
+                            tACL_CONN *p_acl_cb = btm_bda_to_acl(p_ccb->peer_addr, BT_TRANSPORT_BR_EDR);
+                            btm_set_packet_types(p_acl_cb, (btm_cb.btm_acl_pkt_types_supported |
+                                                            HCI_PKT_TYPES_MASK_NO_3_DH1 |
+                                                            HCI_PKT_TYPES_MASK_NO_3_DH3 |
+                                                            HCI_PKT_TYPES_MASK_NO_3_DH5));
+                        }
 
                         /* Check the security */
                         btm_sec_mx_access_request (p_ccb->peer_addr, AVDT_PSM,

AgentFabulous/android_oreo_bacon

(cherry picked from commit 26bb30842b6c64821c2a6c4a515f290140017105)
Merged-In: I4b1b52ec8aeaf5eab0672231889e0ea6f23aa852

stack/avdt/avdt_l2c.c
@@ -33,7 +33,7 @@
 #include "l2cdefs.h"
 #include "btm_api.h"
 #include "btm_int.h"
-
+#include "device/include/interop.h"
 
 /* callback function declarations */
 void avdt_l2c_connect_ind_cback(BD_ADDR bd_addr, uint16_t lcid, uint16_t psm, uint8_t id);
@@ -202,6 +202,15 @@ void avdt_l2c_connect_ind_cback(BD_ADDR bd_addr, uint16_t lcid, uint16_t psm, ui
             p_tbl->state = AVDT_AD_ST_SEC_ACP;
             p_tbl->cfg_flags = AVDT_L2C_CFG_CONN_ACP;
 
+            if (interop_match_addr(INTEROP_2MBPS_LINK_ONLY, (const bt_bdaddr_t *)&bd_addr)) {
+                // Disable 3DH packets for AVDT ACL to improve sensitivity on HS
+                tACL_CONN *p_acl_cb = btm_bda_to_acl(bd_addr, BT_TRANSPORT_BR_EDR);
+                btm_set_packet_types(p_acl_cb, (btm_cb.btm_acl_pkt_types_supported |
+                                                HCI_PKT_TYPES_MASK_NO_3_DH1 |
+                                                HCI_PKT_TYPES_MASK_NO_3_DH3 |
+                                                HCI_PKT_TYPES_MASK_NO_3_DH5));
+            }
+
             /* Check the security */
             rc = btm_sec_mx_access_request (bd_addr, AVDT_PSM,
                 false, BTM_SEC_PROTO_AVDT,
@@ -320,6 +329,15 @@ void avdt_l2c_connect_cfm_cback(uint16_t lcid, uint16_t result)
                         p_tbl->state = AVDT_AD_ST_SEC_INT;
                         p_tbl->lcid = lcid;
                         p_tbl->cfg_flags = AVDT_L2C_CFG_CONN_INT;
+
+                        if (interop_match_addr(INTEROP_2MBPS_LINK_ONLY, (const bt_bdaddr_t *) &p_ccb->peer_addr)) {
+                            // Disable 3DH packets for AVDT ACL to improve sensitivity on HS
+                            tACL_CONN *p_acl_cb = btm_bda_to_acl(p_ccb->peer_addr, BT_TRANSPORT_BR_EDR);
+                            btm_set_packet_types(p_acl_cb, (btm_cb.btm_acl_pkt_types_supported |
+                                                            HCI_PKT_TYPES_MASK_NO_3_DH1 |
+                                                            HCI_PKT_TYPES_MASK_NO_3_DH3 |
+                                                            HCI_PKT_TYPES_MASK_NO_3_DH5));
+                        }
 
                         /* Check the security */
                         btm_sec_mx_access_request (p_ccb->peer_addr, AVDT_PSM,