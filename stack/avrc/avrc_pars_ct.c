bd457b2b2d3bfd199dcfb4a94c305eaaa6344f0c
Pavlin Radoslavov <pavlin@google.com>
2016-08-16 17:59:45 -0700

    Removed bogus AVRC_ADV_CTRL_INCLUDED guard
    Removed bogus "#if (AVRC_ADV_CTRL_INCLUDED == TRUE)"
    inside function AVRC_Ctrl_ParsResponse() for AVRC_OP_VENDOR opcode.
    That guard has been removed by an earlier CL:
      https://android-review.googlesource.com/#/c/213887/
    However, it keeps re-appearing because of the automerger.
    
    Change-Id: Ib73eb1ef7c8245e423cbfbf3d6a0f765ac8ad9ab

--- stack/avrc/avrc_pars_ct.c
+++ stack/avrc/avrc_pars_ct.c
@@ -394,11 +394,9 @@ tAVRC_STS AVRC_Ctrl_ParsResponse (tAVRC_MSG *p_msg, tAVRC_RESPONSE *p_result, ui
     {
         switch (p_msg->hdr.opcode)
         {
-#if (AVRC_ADV_CTRL_INCLUDED == TRUE)
         case AVRC_OP_VENDOR:     /*  0x00    Vendor-dependent commands */
             status = avrc_ctrl_pars_vendor_rsp(&p_msg->vendor, p_result, p_buf,buf_len);
             break;
-#endif
 
         default:
             AVRC_TRACE_ERROR("%s unknown opcode:0x%x", __func__, p_msg->hdr.opcode);
