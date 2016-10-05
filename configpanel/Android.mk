b969ffdd2355e29950007a1c2fce5302ed1363fb
Steve Kondik <steve@cyngn.com>
2016-10-05 01:19:59 -0700

    configpanel: Fix bootup behavior
     * Set the app as direct boot aware so we're able to receive
       the magic broadcast from CMHardwareManager. Otherwise,
       the boot-time initialization never runs.
    
    Change-Id: I0018dd5265ace37e79b775ff62e4f0686f30fd40

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -21,4 +21,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
 LOCAL_PACKAGE_NAME := ConfigPanel
 
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_TAGS := optional
+
 include $(BUILD_PACKAGE)
