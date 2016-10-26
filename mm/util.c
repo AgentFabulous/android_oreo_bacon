09d04da942fefc2822ea96d3d99a26eb6848b07c
nikhil18 <nikhilgolechha1@gmail.com>
2016-10-27 00:26:21 +0530

    Revert "swap: make each swap partition have one address_space"
    This reverts commit ef50b05839031f571662076077aa5ed2cd501795.

--- mm/util.c
+++ mm/util.c
@@ -356,12 +356,9 @@ struct address_space *page_mapping(struct page *page)
 
 	VM_BUG_ON(PageSlab(page));
 #ifdef CONFIG_SWAP
-	if (unlikely(PageSwapCache(page))) {
-		swp_entry_t entry;
-
-		entry.val = page_private(page);
-		mapping = swap_address_space(entry);
-	} else
+	if (unlikely(PageSwapCache(page)))
+		mapping = &swapper_space;
+	else
 #endif
 	if ((unsigned long)mapping & PAGE_MAPPING_ANON)
 		mapping = NULL;
