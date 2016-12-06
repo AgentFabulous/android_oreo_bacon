5ae3d0b7992f1c11c3921a1b8e26620a6079c22c
CheckYourScreen <nimitmehta95@gmail.com>
2016-12-06 13:26:47 +0100

    msm: vidc: use %pK instead of %p which respects kptr_restrict sysctl
    Hide kernel pointers from unprivileged ussers by using %pK format-
    specifier instead of %p. This respects the kptr_restrict sysctl setting
    which is by default on. So by default %pK will print zeroes as address.
    echo 1 to kptr_restrict to print proper kernel addresses.
    
    Signed-off-by: CheckYourScreen <nimitmehta95@gmail.com>
    Signed-off-by: Gianmarco Rengucci <rengucci.gianmarco@gmail.com>

--- drivers/media/platform/msm/vidc/q6_hfi.c
+++ drivers/media/platform/msm/vidc/q6_hfi.c
@@ -345,7 +345,7 @@ static void *q6_hfi_get_device(u32 device_id,
 	int rc = 0;
 
 	if (!callback) {
-		dprintk(VIDC_ERR, "%s Invalid params:  %p\n",
+		dprintk(VIDC_ERR, "%s Invalid params:  %pK\n",
 			__func__, callback);
 		return NULL;
 	}
