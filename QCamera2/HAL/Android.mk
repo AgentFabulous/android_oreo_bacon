90abf55cc6c153ec57c0362f7f7e3236e89a1b75
Gianmarco Rengucci <rengucci.gianmarco@gmail.com>
2016-12-08 22:29:46 +0530

    Hard-code project pathmap.
    Standard paths of AOSPlusone. You may want to change it accordingly to
    your rom.

--- QCamera2/HAL/Android.mk
+++ QCamera2/HAL/Android.mk
@@ -39,8 +39,8 @@ endif
 LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../stack/common \
         frameworks/native/include/media/openmax \
-        $(call project-path-for,qcom-display)/libgralloc \
-        $(call project-path-for,qcom-media)/libstagefrighthw \
+        hardware/qcom/display-caf/msm8974/libgralloc \
+        hardware/qcom/media-caf/msm8974/libstagefrighthw \
         $(LOCAL_PATH)/../../mm-image-codec/qexif \
         $(LOCAL_PATH)/../../mm-image-codec/qomx_core \
         $(LOCAL_PATH)/../util \
@@ -53,9 +53,9 @@ LOCAL_CFLAGS += -DUSE_KK_CODE
 endif
 
 ifeq ($(TARGET_USE_VENDOR_CAMERA_EXT),true)
-LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/msm8974/libgralloc
+LOCAL_C_INCLUDES += hardware/qcom/display-caf/msm8974/libgralloc
 else
-LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
+LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
 endif
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
