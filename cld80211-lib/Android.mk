908546430a3ea89e07ed1868fc38707ec82b0b42
Steven Moreland <smoreland@google.com>
2017-04-10 17:21:45 -0700

    Delete cld80211-lib static variant.
    This isn't used for right now.
    
    Test: doesn't break
    Bug: 36856960
    Change-Id: I8365979a0aab869069997449a4d55e04c9fe63e6

--- cld80211-lib/Android.mk
+++ cld80211-lib/Android.mk
@@ -14,15 +14,3 @@ LOCAL_COPY_HEADERS := cld80211_lib.h
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libcld80211
-LOCAL_CLANG := true
-LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES += $(LOCAL_PATH) \
-	external/libnl/include
-LOCAL_SHARED_LIBRARIES := libcutils libnl liblog
-LOCAL_SRC_FILES := cld80211_lib.c
-LOCAL_CFLAGS += -Wall -Werror -Wno-unused-parameter
-LOCAL_COPY_HEADERS_TO := cld80211-lib
-LOCAL_COPY_HEADERS := cld80211_lib.h
-include $(BUILD_STATIC_LIBRARY)
