8127f7ff8f6d94dc05f7c5d426f498987cc9a363
Sultanxda <sultanxda@gmail.com>
2017-04-12 14:28:36 -0700

    Snap: Hide video pause button
    Currently broken (causes the app to crash), likely due to missing framework
    support.

--- src/com/android/camera/PauseButton.java
+++ src/com/android/camera/PauseButton.java
@@ -51,6 +51,7 @@ public class PauseButton extends ImageView {
         super(context, attrs);
         setClickable(true);
         setSelected(false);
+        setVisibility(View.INVISIBLE);
     }
 
     public void setPaused(boolean paused) {
