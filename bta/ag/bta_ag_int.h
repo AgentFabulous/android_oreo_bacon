c61ef79a794c7ba4edb9b2a5a163fd2b62cf7c44
Zach Johnson <zachoverflow@google.com>
2017-04-18 21:03:07 -0700

    DO NOT MERGE ANYWHERE Allow blocking AG SCO connects by policy
    Needed for devices that support both HFP AG and HFP HF.
    
    btsnoop logs show the sequence of events leading to failure:
    (corroborated by bt logcat)
    
    1. ACL connection with remote phone established on handle 0x01
    2. ACL connection with remote headset established on handle 0x02
    3. Incoming call in HF role from phone
    4. Phone call locally published to telecom
    5. Answer incoming call
    6. SCO connection established with phone in HF role
    7. AG role with headset picks up on call state change to answered
       via BTA_AG_IN_CALL_CONN_RES
    8. Inside handler for BTA_AG_IN_CALL_CONN_RES, bta_ag_sco_open called
       to establish the SCO connection in the AG role with the headset,
       stomping on the previous (wanted) SCO connection with the phone.
    
    Fix is to publish the SCO audio route policy to fluoride, so we can
    stop 8 from happening.
    
    Bug: 32958838
    Test: manual:
          received incoming call in HF role, audio worked both ways;
          recieved incoming MT call, headset audio in AG role worked both ways
    Change-Id: I12961598e8200bd1d5adb46ee8ec3a802114b80a

--- bta/ag/bta_ag_int.h
+++ bta/ag/bta_ag_int.h
@@ -101,7 +101,8 @@ enum
 
     /* these events are handled outside of the state machine */
     BTA_AG_API_ENABLE_EVT,
-    BTA_AG_API_DISABLE_EVT
+    BTA_AG_API_DISABLE_EVT,
+    BTA_AG_API_SET_SCO_ALLOWED_EVT
 };
 
 /* Actions to perform after a SCO event */
@@ -180,6 +181,13 @@ typedef struct
     BT_HDR              hdr;
     tBTA_AG_PEER_CODEC  codec;
 } tBTA_AG_API_SETCODEC;
+
+/* data type for BTA_AG_API_SET_SCO_ALLOWED_EVT */
+typedef struct
+{
+    BT_HDR              hdr;
+    bool                value;
+} tBTA_AG_API_SET_SCO_ALLOWED;
 
 /* data type for BTA_AG_DISC_RESULT_EVT */
 typedef struct
@@ -423,4 +431,5 @@ extern void bta_ag_ci_sco_data(tBTA_AG_SCB *p_scb, tBTA_AG_DATA *p_data);
 extern void bta_ag_set_esco_param(BOOLEAN set_reset, tBTM_ESCO_PARAMS *param);
 extern void bta_ag_ci_rx_data(tBTA_AG_SCB *p_scb, tBTA_AG_DATA *p_data);
 extern void bta_ag_rcvd_slc_ready(tBTA_AG_SCB *p_scb, tBTA_AG_DATA *p_data);
+extern void bta_ag_set_sco_allowed(tBTA_AG_DATA *p_data);
 #endif /* BTA_AG_INT_H */
