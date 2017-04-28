feb2d84f4b69392b5f37ae1ec9d53ac728514275
Philip Cuadra <philipcuadra@google.com>
2017-04-28 01:27:09 +0000

    Revert "Make Bluetooth audio threads use RT scheduling"
    This reverts commit eaa42774f393eb3ccaee91a958e8df6a8fb09b91.
    
    http://b/37733903
    Test:  confirmed switching users does not hang.
    
    Change-Id: Ic54e4ed2dda057e681df225a37a574c221cd8fc3

--- stack/btu/btu_init.cc
+++ stack/btu/btu_init.cc
@@ -36,8 +36,9 @@
 #include "sdpint.h"
 #include "smp_int.h"
 
-// RT priority for audio-related tasks
-#define BTU_TASK_RT_PRIORITY 1
+// Increase BTU task thread priority to avoid pre-emption
+// of audio realated tasks.
+#define BTU_TASK_THREAD_PRIORITY (-19)
 
 extern fixed_queue_t* btif_msg_queue;
 
@@ -129,7 +130,7 @@ void BTU_StartUp(void) {
   bt_workqueue_thread = thread_new(BT_WORKQUEUE_NAME);
   if (bt_workqueue_thread == NULL) goto error_exit;
 
-  thread_set_rt_priority(bt_workqueue_thread, BTU_TASK_RT_PRIORITY);
+  thread_set_priority(bt_workqueue_thread, BTU_TASK_THREAD_PRIORITY);
 
   // Continue startup on bt workqueue thread.
   thread_post(bt_workqueue_thread, btu_task_start_up, NULL);
