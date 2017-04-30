9ccef05b829bb32aeb04305350ea3ff6931b93c2
Sultanxda <sultanxda@gmail.com>
2017-04-30 01:18:57 -0700

    Snap: Make video quality helpers aware of time-lapse mode

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -936,7 +936,9 @@ public class VideoModule implements CameraModule,
 
     private boolean is4KEnabled() {
        if (mProfile.quality == CamcorderProfile.QUALITY_2160P ||
-           mProfile.quality == CamcorderProfile.QUALITY_4KDCI) {
+           mProfile.quality == CamcorderProfile.QUALITY_4KDCI ||
+           mProfile.quality == CamcorderProfile.QUALITY_TIME_LAPSE_2160P ||
+           mProfile.quality == CamcorderProfile.QUALITY_TIME_LAPSE_4KDCI) {
            return true;
        } else {
            return false;
@@ -944,7 +946,8 @@ public class VideoModule implements CameraModule,
     }
 
     private boolean is1080pEnabled() {
-       if (mProfile.quality == CamcorderProfile.QUALITY_1080P) {
+       if (mProfile.quality == CamcorderProfile.QUALITY_1080P ||
+           mProfile.quality == CamcorderProfile.QUALITY_TIME_LAPSE_1080P) {
            return true;
        } else {
            return false;
@@ -952,7 +955,8 @@ public class VideoModule implements CameraModule,
     }
 
     private boolean is720pEnabled() {
-       if (mProfile.quality == CamcorderProfile.QUALITY_720P) {
+       if (mProfile.quality == CamcorderProfile.QUALITY_720P ||
+           mProfile.quality == CamcorderProfile.QUALITY_TIME_LAPSE_720P) {
            return true;
        } else {
            return false;
