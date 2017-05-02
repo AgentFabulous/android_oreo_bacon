bf4e56b6dc63a57e66437f6b2b88ac4736b4523f
Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
2017-05-02 16:24:16 +0530

    Reject LE COC if cid is duplicated
    This is for PTS test TP/LE/CFC/BV-20-C.
    
    The scenario is there is already one COC between DUT and PTS.
    Then PTS tries to establish one more COC with same cid which
    should be rejected.
    
    Test: PTS test TP/LE/CFC/BV-20-C done by Intel (originator
    of this change).
    
    Change-Id: I094a4058ac0e7bb9d7344e66d9feafee17bc3d53
    Signed-off-by: Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
    (cherry picked from commit a252e78152f27ed07285ecf187ace83a12bca06b)

--- stack/l2cap/l2c_ble.cc
+++ stack/l2cap/l2c_ble.cc
@@ -668,6 +668,14 @@ void l2cble_process_sig_cmd(tL2C_LCB* p_lcb, uint8_t* p, uint16_t pkt_len) {
           "initial credit = %d",
           mtu, mps, initial_credit);
 
+      p_ccb = l2cu_find_ccb_by_remote_cid(p_lcb, rcid);
+      if (p_ccb) {
+        L2CAP_TRACE_WARNING("L2CAP - rcvd conn req for duplicated cid: 0x%04x",
+                            rcid);
+        l2cu_reject_ble_connection(p_lcb, id, L2CAP_LE_SOURCE_CID_ALREADY_ALLOCATED);
+        break;
+      }
+
       p_rcb = l2cu_find_ble_rcb_by_psm(con_info.psm);
       if (p_rcb == NULL) {
         L2CAP_TRACE_WARNING("L2CAP - rcvd conn req for unknown PSM: 0x%04x",
