fa1c03c8d01721f81c2ca9b86142a6fce7f10924
Steve Paik <spaik@google.com>
2017-05-03 12:18:35 -0700

    Fix build for bee, gordon_peak, and owl
    Bug: 37852218
    Test:  bee, gordon_peak, and owl are green
    Change-Id: I7e77b14ee9bbcb9c404be211f3a71639ba332ddb

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
-# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
-ifneq ($(BOARD_IS_AUTOMOTIVE),true)
+# TODO:  Fix this properly when b/37901207 is fixed
+#ifneq ($(BOARD_IS_AUTOMOTIVE),true)
+ifeq ($(filter bat,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles)
 endif
