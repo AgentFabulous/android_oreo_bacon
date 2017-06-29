0d3e632102b93919420c9005a675b39a734accb1
Suman Mukherjee <sumam@codeaurora.org>
2017-06-29 17:22:35 +0530

    QCamera2: mm-camera-interface: Camera Daemon late start issue fix
    In Android O, Cameraprovider service is introduced newly and its
    starting before main service which results in getnumofcameras failure.
    Added re-try count to fix this issue.
    
    Change-Id: I3a3ee1cbe6ad983d5ad7266f154b33b93d303804
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- QCamera2/stack/mm-camera-interface/src/mm_camera_interface.c
+++ QCamera2/stack/mm-camera-interface/src/mm_camera_interface.c
@@ -1275,6 +1275,7 @@ uint8_t get_num_of_cameras()
 {
     int rc = 0;
     int dev_fd = 0;
+    int i = 0;
     struct media_device_info mdev_info;
     int num_media_devices = 0;
     uint8_t num_cameras = 0;
@@ -1351,7 +1352,15 @@ uint8_t get_num_of_cameras()
     cfg.cfgtype = CFG_SINIT_PROBE_WAIT_DONE;
     cfg.cfg.setting = NULL;
     if (ioctl(sd_fd, VIDIOC_MSM_SENSOR_INIT_CFG, &cfg) < 0) {
-        CDBG_ERROR("failed");
+        CDBG("failed...Camera Daemon may not up so try again");
+        for(i = 0; i < MM_CAMERA_EVT_ENTRY_MAX; i++) {
+            if (ioctl(sd_fd, VIDIOC_MSM_SENSOR_INIT_CFG, &cfg) < 0) {
+                CDBG("failed...Camera Daemon may not up so try again");
+                continue;
+            }
+            else
+                break;
+        }
     }
     close(sd_fd);
     dev_fd = 0;
