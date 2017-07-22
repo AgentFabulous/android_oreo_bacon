c2862c5f909d17a6308d86d9c5e0c568786d8341
Santhosh Behara <santhoshbehara@codeaurora.org>
2017-07-22 02:45:57 -0700

    mm-video-v4l2: venc: Protect buffer from being freed while accessing
    Output buffer(in use-buffer mode) has an internal backup ion buffer.
    The contents of this buffer are deep-copied in client's buffer in
    the context of VideoEncCallBackThread; while this buffer can be
    freed in the client thread's context.
    Check the allocation bitmask before attempting to copy and
    synchronize these operations by holding a lock
    
    Fixes bug 36130225
     Security Vulnerability - Heap use after free in libOmxVenc
    
    CRs-Fixed: 2053101
    
    Bug: 36130225
    Change-Id: If5e89703b2dec0aee8acb7e897e9df94227af3f3
    Author: Praveen Chavan<pchavan@codeaurora.org>
    (cherry picked from commit 0008e212118f9172e0e6cbafd679ce0a42cbf019)

--- msm8974/mm-video-v4l2/vidc/venc/src/omx_video_encoder.cpp
+++ msm8974/mm-video-v4l2/vidc/venc/src/omx_video_encoder.cpp
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-Copyright (c) 2010-2014, The Linux Foundation. All rights reserved.
+Copyright (c) 2010-2017, The Linux Foundation. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -2255,11 +2255,18 @@ int omx_venc::async_message_process (void *context, void* message)
                     OMX_COMPONENT_GENERATE_EBD);
             break;
         case VEN_MSG_OUTPUT_BUFFER_DONE:
+        {
             omxhdr = (OMX_BUFFERHEADERTYPE*)m_sVenc_msg->buf.clientdata;
+            OMX_U32 bufIndex = (OMX_U32)(omxhdr - omx->m_out_mem_ptr);
 
             if ( (omxhdr != NULL) &&
-                    ((OMX_U32)(omxhdr - omx->m_out_mem_ptr)  < omx->m_sOutPortDef.nBufferCountActual)) {
-                if (m_sVenc_msg->buf.len <=  omxhdr->nAllocLen) {
+                    (bufIndex  < omx->m_sOutPortDef.nBufferCountActual)) {
+                auto_lock l(omx->m_buf_lock);
+                if (BITMASK_ABSENT(&(omx->m_out_bm_count), bufIndex)) {
+                    DEBUG_PRINT_ERROR("Recieved FBD for buffer that is already freed !");
+                    break;
+                }
+                if (!omx->is_secure_session() && (m_sVenc_msg->buf.len <=  omxhdr->nAllocLen)) {
                     omxhdr->nFilledLen = m_sVenc_msg->buf.len;
                     omxhdr->nOffset = m_sVenc_msg->buf.offset;
                     omxhdr->nTimeStamp = m_sVenc_msg->buf.timestamp;
@@ -2284,6 +2291,7 @@ int omx_venc::async_message_process (void *context, void* message)
             omx->post_event ((unsigned long)omxhdr,m_sVenc_msg->statuscode,
                     OMX_COMPONENT_GENERATE_FBD);
             break;
+        }
         case VEN_MSG_NEED_OUTPUT_BUFFER:
             //TBD what action needs to be done here??
             break;
