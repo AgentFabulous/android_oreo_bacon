e48939f7fa3b42ffdfb63e55ba71578fd2d80e27
Kshitij Gupta <kshitijgm@gmail.com>
2017-08-13 13:40:07 +0530

    Revert "Remove post-install verification"
    - This is needed for flashing the zip as a Secondary ROM
      (MultiROM)
    
    This reverts commit b2deb220.
    
    Change-Id: Iffcb77f89f1d95f827b5bf6cf0160550b5397b13
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1394,8 +1394,6 @@ class BlockDifference(object):
     if progress:
       script.ShowProgress(progress, 0)
     self._WriteUpdate(script, output_zip)
-    if OPTIONS.verify:
-      self._WritePostInstallVerifyScript(script)
 
   def WriteStrictVerifyScript(self, script):
     """Verify all the blocks in the care_map, including clobbered blocks.
@@ -1546,8 +1544,7 @@ class BlockDifference(object):
 
     call = ('block_image_update("{device}", '
             'package_extract_file("{partition}.transfer.list"), '
-            '"{partition}.new.dat", "{partition}.patch.dat") ||\n'
-            '  abort("E{code}: Failed to update {partition} image.");'.format(
+            '"{partition}.new.dat", "{partition}.patch.dat");\n'.format(
                 device=self.device, partition=self.partition, code=code))
     script.AppendExtra(script.WordWrap(call))
 
