4240c886eb07f4715e1f996363ecbf8308a46da8
Adrian DC <radian.dc@gmail.com>
2017-09-03 11:20:06 +0000

    camera: Upgrade library dependencies for AOSP 8.0.0
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- QCamera2/HAL/Android.mk
+++ QCamera2/HAL/Android.mk
@@ -44,6 +44,7 @@ endif
 
 LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../stack/common \
+        framework/native/include \
         frameworks/native/include/media/openmax \
         hardware/qcom/display/msm8994/libgralloc \
         hardware/qcom/media/msm8974/libstagefrighthw \
@@ -64,7 +65,8 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libgui
-LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface
+LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libhidltransport libsensor android.hidl.token@1.0-utils
+LOCAL_STATIC_LIBRARIES := libarect
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
